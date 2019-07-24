0290141dbe78a3ab0ddd6fb96e991302a5ae25ff
Otávio Goes <gord@NiNNo.localdomain>
2019-07-24 12:33:18 -0300

    base

---
+++ src/database/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+};
